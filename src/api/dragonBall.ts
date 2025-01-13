c85409f2fefd5a9c10fb7af72335c7cd2138c7bf
aitorortega03 <aitorortega21@gmail.com>
2025-01-13 19:09:45 +0100

    Fix fetch to api response structure

--- src/api/dragonBall.ts
+++ src/api/dragonBall.ts
@@ -1,12 +1,20 @@
-const API_BASE_URL = "https://dragonball-api.com/api"
+import { Character } from "../types";
+
+const API_BASE_URL = "https://dragonball-api.com/api";
 
 export async function fetchCharacters() {
   try {
     const response = await fetch(`${API_BASE_URL}/characters`);
+    if (!response.ok) throw new Error(`HTTP Error: ${response.status}`);
+
     const data = await response.json();
-    return data;
+    if (data.items) {
+      return data.items as Character[];
+    } else {
+      throw new Error("Unexpected API response format.");
+    }
   } catch (error) {
-    console.error('Failed to fetch characters:', error);
+    console.error("Failed to fetch characters:", error);
     return null;
   }
 }
