382675fb924357d414ec0f9242cfb2140b01f4e0
aitorortega03 <aitorortega21@gmail.com>
2025-01-17 14:11:48 +0100

    Add planet list

--- src/api/dragonBall.ts
+++ src/api/dragonBall.ts
@@ -1,4 +1,4 @@
-import { Character } from "../types";
+import { Character, Planet } from "../types";
 
 const API_BASE_URL = "https://dragonball-api.com/api";
 
@@ -15,6 +15,23 @@ export async function fetchCharacters() {
     }
   } catch (error) {
     console.error("Failed to fetch characters:", error);
+    return null;
+  }
+}
+
+export async function fetchPlanets() {
+  try {
+    const response = await fetch(`${API_BASE_URL}/planets`);
+    if (!response.ok) throw new Error(`HTTP Error: ${response.status}`);
+
+    const data = await response.json();
+    if (data.items) {
+      return data.items as Planet[];
+    } else {
+      throw new Error("Unexpected API response format.");
+    }
+  } catch (error) {
+    console.error("Failed to fetch planets:", error);
     return null;
   }
 }
---
+++ src/components/PlanetList.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import { Planet } from "../types";
+import { fetchPlanets } from "../api/dragonBall";
+
+const PlanetList: React.FC = () => {
+  const [planets, setPlanets] = useState<Planet[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const getPlanets = async () => {
+      const data = await fetchPlanets();
+      if (data) {
+        setPlanets(data);
+      } else {
+        setError("Failed to load the planets. Please try again.")
+      }
+      setLoading(false);
+    };
+
+    getPlanets();
+  }, []);
+
+  if (loading) return <div className="text-accent text-center">Loading...</div>;
+  if (error) return <div className="text-secondary text-center">{error}</div>;
+  if (planets.length === 0) return <div className="text-center text-accent">No planets available.</div>;
+
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
+      {planets.map((planet) => (
+        <div key={planet.id} className="bg-light text-dark rounded shadow p-4">
+          <img
+          src={planet.image}
+          alt={planet.name}
+          className="h-60 w-60 mx-auto object-contain"
+        />
+          <h2 className="text-center mt-2 text-primary font-name text-3xl font-bold">{planet.name}</h2>
+        </div>
+      ))}
+    </div>
+  )
+}
+export default PlanetList;
